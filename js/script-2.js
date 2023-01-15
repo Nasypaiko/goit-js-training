8add6008c56d64619ebac4a63f0153f1993ad361
Nasypaiko <b.nasypaiko@gmail.com>
2023-01-15 22:54:41 +0100

    Update js

---
+++ js/script-2.js
@@ -0,0 +1,28 @@
+// Парс числа с Number.parseInt() и Number.parseFloat()
+
+let elementWidth = '50px';
+const result = Number.parseInt(elementWidth);
+console.log(result);
+
+let elementHeight = '200.74';
+elementHeight = Number.parseFloat(elementHeight);
+console.log('elementHeight: ', elementHeight);
+
+// Метод число.toFixed(digits)
+
+let salary = 1300.6543;
+salary = Number(salary.toFixed(2));
+// salary = Number(salary);
+console.log(salary);
+
+// Приведение (преобразование) к числу с Number(value)
+// Значение NaN (Not a Number) и метод Number.isNaN(value)
+
+let quantity = '30';
+let value = 'Эту строку невозможно привести к числу';
+
+console.log(Number(true));
+console.log(Number(false));
+
+console.log(Number(quantity));
+console.log(Number(value));
---
+++ js/script-3.js
@@ -0,0 +1,64 @@
+// Объект Math
+// Возведение в степень
+// Exponent operator
+
+// console.log(Math);
+
+const first = 2;
+const second = 5;
+
+const result = Math.pow(2, 3);
+
+console.log(result);
+
+const base = 2;
+const power = 5;
+
+console.log(Math.pow(base, power));
+
+console.log(2 ** 5);
+
+// Math.sqrt()
+
+// Напиши скрипт который просит пользователя ввести число и степень,
+// возводит число в эту степень и выводит результат в консоль
+
+// 1. попросить ввести число и сохранить в переменную
+
+let bases = prompt('Введите число');
+bases = Number(bases);
+console.log(bases);
+
+// 2. попросить ввести степень и сохранить в переменную
+
+let powers = prompt('Введите степень');
+powers = Number(powers);
+console.log(powers)
+
+// 3. возвести введённые данные в степень и вывести
+
+const results = bases ** powers;
+console.log(results);
+
+// Генерим псевдослучайные числа
+// Math.random()
+// Math.round()
+
+console.log(Math.random());
+
+// Math.random() * (max - min) + min;
+
+const max = 100;
+const min = 0;
+const resultat = Math.round(Math.random() * (max - min) + min);
+console.log(resultat);
+
+// --- Задача ---
+
+const colors = ['tomato', 'teal', 'orange', 'deeppink', 'skyblue'];
+const maxs = colors.length - 1;
+const mins = 0;
+const index = Math.round(Math. random() * (maxs- mins) + min);
+const color = colors[index];
+console.log(color);
+// document.body.style.backgroundColor = color;
