5baee9bb7177d58cfd6e88540522e3c93d7f590d
Nasypaiko <b.nasypaiko@gmail.com>
2023-01-17 20:07:28 +0100

    button

--- js/button.js
+++ js/button.js
@@ -1,22 +1,27 @@
 const btnAdd = document.querySelector('button[data-add]');
+const resetBtn = document.querySelector('button[data-reset]');
 const valueInput = document.querySelector('input[data-value');
+const outputEl = document.querySelector('.js-output span');
 
 let total = 0;
 
 // console.log(btnAdd);
 // console.log(valueInput);
 
-btnAdd.textContent = 'Кнопка'
+btnAdd.textContent = 'Кнопка прибавить'
 
 btnAdd.addEventListener('click', function () {
     console.log('click me please again')
 
     const value = Number(valueInput.value);
-    console.log(value);
-
+    // console.log(value);
     total += value;
-
-    console.log('total: ', total);
-
+    outputEl.textContent = total;
+    // console.log('total: ', total);
     valueInput.value = '';
+});
+
+resetBtn.addEventListener('click', function () {
+    total = 0;
+    outputEl.textContent = total;
 })
