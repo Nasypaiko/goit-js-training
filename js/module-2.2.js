6de20b4a16a9b5c9bd05727af0a46dd156358035
Nasypaiko <b.nasypaiko@gmail.com>
2023-01-30 14:19:44 +0100

    module 3

--- js/module-2.2.js
+++ js/module-2.2.js
@@ -1,41 +1,47 @@
-function calculateEngravingPrice(message, pricePerWord) {
-  let words = message.split(" ");
+// function calculateEngravingPrice(message, pricePerWord) {
+//   let words = message.split(" ");
 
-  return words.length * pricePerWord;
-}
-
-
-
-function slugify(title) {
-  const normalizedTitle = title.toLowerCase();
-
-  const words = normalizedTitle.split(" ");
-
-  const slug = words.join("-");
-
-  // const slug = title.toLowerCase().split(' ').join('-');
-
-  return slug;
-}
+//   return words.length * pricePerWord;
+// }
 
 
-function calculateTotal(number) {
 
-let sum = 0;
-  for (let i = 0; i <= number; i += 1) {
+// function slugify(title) {
+//   const normalizedTitle = title.toLowerCase();
 
-      sum += i 
-  }
+//   const words = normalizedTitle.split(" ");
 
-  return sum
-}
+//   const slug = words.join("-");
 
+//   // const slug = title.toLowerCase().split(' ').join('-');
 
-const fruits = ['apple', 'plum', 'pear', 'orange'];
-
-for (let i = 0; i < fruits.length; i += 1) { 
-  const fruit = fruits[i]; 
-  console.log(fruit);
-}
+//   return slug;
+// }
 
 
+// function calculateTotal(number) {
+
+// let sum = 0;
+//   for (let i = 0; i <= number; i += 1) {
+
+//       sum += i 
+//   }
+
+//   return sum
+// }
+
+
+// const fruits = ['apple', 'plum', 'pear', 'orange'];
+
+// for (let i = 0; i < fruits.length; i += 1) { 
+//   const fruit = fruits[i]; 
+//   console.log(fruit);
+// }
+
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+fruits[1] = "peach"
+fruits[3] = "banana"
+
+console.log(fruits)
