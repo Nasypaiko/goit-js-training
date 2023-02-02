d1bc4ef7a39282439e5b87d2550a47927628a7ba
Nasypaiko <b.nasypaiko@gmail.com>
2023-02-02 13:49:53 +0100

    module 3

--- js/module-2.2.js
+++ js/module-2.2.js
@@ -75,3 +75,80 @@
 
 
 
+// function makeArray(firstArray, secondArray, maxLength) {
+
+// return firstArray.concat(secondArray).length > maxLength ?
+//   firstArray.concat(secondArray).slice(0,maxLength) :
+//   firstArray.concat(secondArray);
+
+// }
+  
+// console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4))
+
+
+
+
+// const apartment = {
+//   imgUrl: "https://via.placeholder.com/640x480",
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+//   tags: ["premium", "promoted", "top"],
+//   owner: {
+//     name: "Henry",
+//     phone: "982-126-1588",
+//     email: "henry.carter@aptmail.com",
+//   },
+// };
+
+// const ownerName = apartment.owner.name;
+// const ownerPhone = apartment.owner.phone;
+// const ownerEmail = apartment.owner.email;
+// const numberOfTags = apartment.tags.length;
+// const firstTag = apartment.tags.slice(0);
+// const lastTag = apartment.tags.slice(2);
+
+// console.log(apartment)
+
+
+
+
+// const apartment = {
+//   descr: "Spacious apartment in the city center",
+//   rating: 4,
+//   price: 2153,
+// };
+// const values = [];
+
+// const keys = Object.keys(apartment);
+
+// for (const key of keys) {
+//   values.push(apartment[key])
+// }
+
+
+
+
+// function add(...args) {
+//   let sum = 0
+//   for (const arg of args) {
+//     sum += arg
+//   }
+//   return sum
+// }
+
+// ------ Сложение только тех чисел которые больше перового
+
+// function addOverNum(...args) {
+//   let total = 0;
+
+//   for (const arg of args) {
+//     if (arg > args[0])
+//     total += arg;
+//   }
+
+//   return total;
+// }
+
+
+
